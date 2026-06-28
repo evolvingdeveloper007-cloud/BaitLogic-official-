fbd0e5d56c12f522df2bf0e24d80531113230038
evolvingdeveloper007-cloud <evolving.developer007@gmail.com>
2026-06-28 07:07:35 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+window.BAITLOGIC_CONFIG = {
+  SUPABASE_URL: "https://khhishscjirjxhsulniq.supabase.co",
+  SUPABASE_ANON_KEY:
+    "sb_publishable_d-MPFEIo3g9ORJXFzHiKtA_sIBHHD_7",
+  SITE_NAME: "BaitLogic",
+  DOMAIN: "https://baitlogic.org"
+};
