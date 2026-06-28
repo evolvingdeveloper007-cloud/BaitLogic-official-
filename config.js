aca53e28f0eb9f8bca551a31830e6d6988e9b177
evolvingdeveloper007-cloud <evolving.developer007@gmail.com>
2026-06-28 07:37:17 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.BAITLOGIC_CONFIG = {
-  SUPABASE_URL: "https://khhishscjirjxhsulniq.supabase.co",
-  SUPABASE_ANON_KEY:
-    "sb_publishable_d-MPFEIo3g9ORJXFzHiKtA_sIBHHD_7",
-  SITE_NAME: "BaitLogic",
-  DOMAIN: "https://baitlogic.org"
-};
