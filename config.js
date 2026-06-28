2e7306d014ecb3384d80da2387bbc7caaf6bbb03
evolvingdeveloper007-cloud <evolving.developer007@gmail.com>
2026-06-28 08:58:30 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+window.BAITLOGIC_CONFIG = {
+  mode: 'live',
+
+  siteName: 'BaitLogic',
+
+  supabaseUrl: 'https://khhishscjirjxhsulniq.supabase.co',
+
+  supabaseAnonKey: 'sb_publishable_d-MPFEIo3g9ORJXFzHiKtA_sIBHHD_7',
+
+  adminRedirectUrl: 'https://baitlogic.pages.dev/admin',
+
+  adminEmails: [
+    'baitlogic@outlook.com'
+  ]
+};
