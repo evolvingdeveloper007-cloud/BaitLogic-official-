5b97dd2c2014a608a169be19f186b087f8e4ce84
evolvingdeveloper007-cloud <evolving.developer007@gmail.com>
2026-06-28 07:37:51 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.BAITLOGIC_CONFIG = {
+  mode: "live",
+  supabaseUrl: "https://khhishscjirjxhsulniq.supabase.co",
+  supabaseAnonKey: "sb_publishable_d-MPFEIo3g9ORJXFzHiKtA_sIBHHD_7",
+
+  SUPABASE_URL: "https://khhishscjirjxhsulniq.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_d-MPFEIo3g9ORJXFzHiKtA_sIBHHD_7"
+};
